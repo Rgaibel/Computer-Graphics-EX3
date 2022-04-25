f783c74813a97417b2a4981bc72a68afe5321bef
Rgaibel <rgaibel@gmail.com>
2022-04-25 18:49:17 +0300

    ammended the sphere function, it now works, but it returns spheres that are too large

--- helper_classes.py
+++ helper_classes.py
@@ -173,12 +173,12 @@ class Sphere(Object3D):
     def intersect(self, ray: Ray):
         b = 2 * np.dot(ray.direction, ray.origin - self.center)
         c = np.linalg.norm(ray.origin - self.center) * 2 - self.radius * 2
-        d = b ** 2 - 4*c
-        if d > 0:
-            t1 = (-b + np.sqrt(d)) / 2
-            t2 = (-b - np.sqrt(d)) / 2
+        delta = b ** 2 - 4*c
+        if delta > 0:
+            t1 = (-b + np.sqrt(delta)) / 2
+            t2 = (-b - np.sqrt(delta)) / 2
             if t1 > 0 and t2 > 0:
-                return min(t1, t2)
+                return min(t1, t2), self
         return None
 
     def compute_normal(self, intersection):
